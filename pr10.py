a442e1625b6ebc3d564437189dddb38294b63db0
varsha <51949295+varsha126@users.noreply.github.com>
2019-07-10 17:04:57 +0530

    Update pr10.py

--- pr10.py
+++ pr10.py
@@ -1,7 +1,7 @@
 cha2=int(input())
 cho2=[int(o) for o in input().split(" ")]
 che2=0
-for p in range(cha2):
+for d in range(cha2):
       for f in range(d):
            if(cho2[f]<cho2[d]):
                 che2+=cho2[f]

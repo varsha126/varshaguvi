cb5063305cba4b20338f6cdde26d08f33cffc4c1
varsha <51949295+varsha126@users.noreply.github.com>
2019-07-20 14:53:34 +0530

    Update pr17.py

--- pr17.py
+++ pr17.py
@@ -1,1 +1,16 @@
-
+np1=input()
+a=list(map(int,np1.split()))
+k=a[1]
+ha=input()
+flag=0
+sa=list(map(int,ha.split()))
+for i in range(0,len(sa)-1):
+	for j in range(i+1,len(sa)):
+		if sa[i]+sa[j]==k:
+			print("yes")
+			flag=1
+			break
+	if flag==1:
+		break
+if flag!=1:
+	print("no")

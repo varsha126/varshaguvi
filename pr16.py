2418c5762a9968d2d52aab750b3a47a517f1f946
varsha <51949295+varsha126@users.noreply.github.com>
2019-07-20 14:48:57 +0530

    Update pr16.py

--- pr16.py
+++ pr16.py
@@ -1,1 +1,12 @@
 
+dn1=int(input())
+lsi1=list(map(int,input().split()))
+an=[1]*dn1
+for i in range(dn1):
+    if(i==0):
+        if(lsi1[i]>lsi1[i+1]):
+            an[i]=an[i]+an[i+1]
+    elif(i>0):
+        if(lsi1[i]>lsi1[i-1]):
+            an[i]=an[i]+an[i-1]
+print(sum(an))
